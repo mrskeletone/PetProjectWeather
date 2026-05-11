0bfb0039b280cdf93ef0acc9aa333c5c813a280b
mrskeletone <mr_skeletone_1000@mail.ru>
2026-05-11 15:23:01 +0300

    First commit

---
+++ src/main/java/org/example/petprojectweather/PetProjectWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.example.petprojectweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/petprojectweather/PetProjectWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.petprojectweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
